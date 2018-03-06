9c76c6db8b2a7b4352cba072c240319f4f79ca3c
Michael Ennen <mike.ennen@gmail.com>
2018-03-05 20:41:00 -0700

    Hack a quick git to hg patch conversion.

--- src/main/java/org/javafxports/jfxmirror/Handler.java
+++ src/main/java/org/javafxports/jfxmirror/Handler.java
@@ -1,8 +1,20 @@
 package org.javafxports.jfxmirror;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.Scanner;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.script.SimpleScriptContext;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
@@ -33,12 +45,20 @@ public class Handler implements RequestStreamHandler {
         context.getLogger().log("pullRequestEvent: " + pullRequestEvent);
 
         // Check if username has signed OCA.
-
         if (action.equalsIgnoreCase("opened")) {
             checkContributorAgreement(username);
         }
 
+        // See if there is a JBS bug associated with this PR. (How? PR labels? Don't think they provide a way to set a message)
 
+        // Convert the pull-request from a git patch to an hg patch.
+        try {
+            String hgPatch = convertGitPatchToHgPatch(patchUrl);
+        } catch (ScriptException e) {
+            context.getLogger().log("error: " + e.getMessage());
+        }
+
+        // Use the hg patch to generate a webrev on the latest master of upstream OpenJFX.
     }
 
     private static void checkContributorAgreement(String username) {
@@ -46,4 +66,19 @@ public class Handler implements RequestStreamHandler {
     }
 
 
+    /**
+     * Takes as input the URL to a git patch and returns the equivalent hg patch.
+     */
+    private static String convertGitPatchToHgPatch(String patchUrl) throws ScriptException, IOException {
+        String gitPatch = new Scanner(new URL(patchUrl).openStream(), "UTF-8").useDelimiter("\\A").next();
+        StringWriter scriptOut = new StringWriter();
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptContext context = new SimpleScriptContext();
+        context.setWriter(scriptOut);
+        context.setReader(new StringReader(gitPatch));
+        ScriptEngine engine = manager.getEngineByName("python");
+        engine.eval(new BufferedReader(new InputStreamReader(
+                Handler.class.getResourceAsStream("/git-patch-to-hg-patch"))), context);
+        return scriptOut.toString();
+    }
 }
